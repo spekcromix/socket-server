4b338d6e4cb9f20a7c32f06245291749717c2c0b
rodolfole <rodolfolarae@gmail.com>
2019-01-29 21:10:08 -0600

    Seccion 4

---
+++ sockets/socket.ts
@@ -0,0 +1,15 @@
+import { Socket } from 'socket.io';
+
+export const desconectar = (cliente: Socket) => {
+  cliente.on('disconnect', () => {
+    console.log('Cliente Desconectado');
+  });
+};
+
+export const mensaje = (cliente: Socket, io: SocketIO.Server) => {
+  cliente.on('mensaje', (payload: { de: string; cuerpo: string }) => {
+    console.log('Mensaje recibido', payload);
+
+    io.emit('mensaje-nuevo', payload);
+  });
+};
